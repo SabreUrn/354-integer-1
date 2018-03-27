1673e4cb06632ba954aa9d89530ba36c9d22f497
SabreUrn <no@way.com>
2018-03-27 17:13:50 +0200

    initial commit

---
+++ 354-integer-1/Integer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _354_integer_1 {
+	public class Integer {
+		Dictionary<long, long> _factors;
+
+		public Integer() {
+			_factors = new Dictionary<long, long>();
+		}
+
+		public long FindSumOfFactors(long a) {
+			//ensure a is positive
+			if (a <= 0) {
+				Console.WriteLine("Product must be a positive integer.");
+				return -1;
+			}
+
+			long tempSum = 0;
+			long sum = a+1; //will always be the sum of the first factorisation (a * 1) and other correct sums will always be lower than or equal to
+			for(long i = 1; i <= Math.Sqrt(a); i++) { //after the sqrt of a, we're past the half-way point. all pairs afterwards are duplicates.
+				if(a % i == 0) {			//if a divides with i with no remainder,
+					tempSum = a / i + i;	//set the value to check against to the sum of i and a / i
+				}
+				if(tempSum <= sum) {
+					sum = tempSum;
+				} else {
+					break;				//we're going the other way now, break the for-loop
+				}
+			}
+			//DebugPrintDictionaryContents();
+			return sum;
+		}
+
+		private void DebugPrintDictionaryContents() {
+			foreach (KeyValuePair<long, long> kvp in _factors) {
+				Console.WriteLine(kvp.Key + ", " + kvp.Value);
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ 354-integer-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _354_integer_1 {
+	class Program {
+		static void Main(string[] args) {
+			Integer integer = new Integer();
+			Console.WriteLine(integer.FindSumOfFactors(1234567891011));
+
+			Console.ReadKey();
+		}
+	}
+}
